32defec54da1bc3ee731811ac4de4cd73a99612d
Akash Kumar <121351972+akashkr16@users.noreply.github.com>
2025-09-04 22:37:53 +0530

    Create 02_01_iv_shellsort.c

---
+++ 02_01_iv_shellsort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void shellSort(int arr[], int n) {
+    for (int gap = n/2; gap > 0; gap /= 2) {
+        for (int i = gap; i < n; i++) {
+            int temp = arr[i], j;
+            for (j = i; j >= gap && arr[j-gap] > temp; j -= gap) {
+                arr[j] = arr[j-gap];
+            }
+            arr[j] = temp;
+        }
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+
+    printf("Original Array: ");
+    printArray(arr, n);
+    shellSort(arr, n);
+    printf("Sorted Array (Shell Sort): ");
+    printArray(arr, n);
+    return 0;
+}
