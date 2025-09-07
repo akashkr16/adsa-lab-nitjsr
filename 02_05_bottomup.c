964201aa6ecce9c9d42bf2dc0edaca72f90958af
Akash Kumar <121351972+akashkr16@users.noreply.github.com>
2025-09-07 22:32:48 +0530

    Create 02_05_bottomup.c

---
+++ 02_05_bottomup.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+void merge(int arr[], int l, int m, int r) {
+    int n1 = m - l + 1, n2 = r - m;
+    int L[n1], R[n2];
+
+    for (int i = 0; i < n1; i++) L[i] = arr[l + i];
+    for (int j = 0; j < n2; j++) R[j] = arr[m + 1 + j];
+
+    int i = 0, j = 0, k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) arr[k++] = L[i++];
+        else arr[k++] = R[j++];
+    }
+    while (i < n1) arr[k++] = L[i++];
+    while (j < n2) arr[k++] = R[j++];
+}
+
+void mergeSortIterative(int arr[], int n) {
+    int curr_size; // size of subarrays
+    int left_start; 
+
+    for (curr_size = 1; curr_size <= n - 1; curr_size = 2 * curr_size) {
+        for (left_start = 0; left_start < n - 1; left_start += 2 * curr_size) {
+            int mid = left_start + curr_size - 1;
+            int right_end = (left_start + 2 * curr_size - 1 < n - 1) ? (left_start + 2 * curr_size - 1) : (n - 1);
+
+            if (mid < right_end) {
+                merge(arr, left_start, mid, right_end);
+            }
+        }
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+
+    printf("Original Array: ");
+    printArray(arr, n);
+
+    mergeSortIterative(arr, n);
+
+    printf("Sorted Array (Iterative Merge Sort): ");
+    printArray(arr, n);
+
+    return 0;
+}
